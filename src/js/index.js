4c39a81db55715ecc416d6b813a32a47cea3e878
Safarov <elkhan.safarov10@yandex.com>
2020-11-11 21:10:35 +0500

    Подключение стилей

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,1 @@
-import '../styles/less.less'
-
-function revers(text) {
-    return text.split('').reverse().join('');
-}
-console.log('fewfewfewfewfew', revers('Hello world'))
+import '../styles/style.scss'
