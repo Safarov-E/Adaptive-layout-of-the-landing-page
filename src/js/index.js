2d229464872ded6728f16e68f5e6309acea3a593
Safarov <elkhan.safarov10@yandex.com>
2020-11-14 19:19:43 +0500

    Слайдер

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../styles/style.scss';
 import $ from "jquery";
+import 'slick-carousel'
 
 $('.wrapper').addClass('loaded');
 $('.icon__menu').click(function(event) {
@@ -15,3 +16,20 @@ function ibg() {
     })
 }
 ibg();
+
+if($('.slider__body').length > 0) {
+    $('.slider__body').slick({
+        dots: true,
+        arrows: false,
+        accessibility: false,
+        slidesToShow: 1,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {}
+        }]
+    })
+}
